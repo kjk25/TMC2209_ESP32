31277022f6af7e4d4074ddf0d3fe945441dd19f9
Kai Klein <email@kaiklein.de>
2021-09-18 11:15:16 +0200

    Add files via upload

---
+++ test_script_03_basic_movement.py
@@ -0,0 +1,123 @@
+from tmc.TMC_2209_StepperDriver import *
+import time
+
+
+print("---")
+print("SCRIPT START")
+print("---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# initiate the TMC_2209 class
+# use your pins for pin_step, pin_dir, pin_en here
+#-----------------------------------------------------------------------
+tmc = TMC_2209(27, 14, 26)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the loglevel of the libary (currently only printed)
+# set whether the movement should be relative or absolute
+# both optional
+#-----------------------------------------------------------------------
+tmc.setLoglevel(Loglevel.debug)
+tmc.setMovementAbsRel(MovementAbsRel.relative)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# these functions change settings in the TMC register
+#-----------------------------------------------------------------------
+tmc.setDirection_reg(False)
+tmc.setVSense(True)
+tmc.setCurrent(200)
+tmc.setIScaleAnalog(True)
+tmc.setInterpolation(True)
+tmc.setSpreadCycle(False)
+tmc.setMicrosteppingResolution(1)
+tmc.setInternalRSense(False)
+
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# these functions read and print the current settings in the TMC register
+#-----------------------------------------------------------------------
+#tmc.readIOIN()
+#tmc.readCHOPCONF()
+#tmc.readDRVSTATUS()
+#tmc.readGCONF()
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the Accerleration and maximal Speed
+#-----------------------------------------------------------------------
+tmc.setAcceleration(2000)
+tmc.setMaxSpeed(500)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# activate the motor current output
+#-----------------------------------------------------------------------
+tmc.setMotorEnabled(True)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# move the motor 1 revolution
+#-----------------------------------------------------------------------
+tmc.runToPositionSteps(800)                             #move to position 400
+tmc.runToPositionSteps(0)                               #move to position 0
+
+
+tmc.runToPositionSteps(1000, MovementAbsRel.relative)    #move 400 steps forward
+tmc.runToPositionSteps(-600, MovementAbsRel.relative)   #move 400 steps backward
+
+
+tmc.runToPositionSteps(400)                             #move to position 400
+tmc.runToPositionSteps(0)                               #move to position 0
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deactivate the motor current output
+#-----------------------------------------------------------------------
+tmc.setMotorEnabled(False)
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deinitiate the TMC_2209 class
+#-----------------------------------------------------------------------
+del tmc
+
+print("---")
+print("SCRIPT FINISHED")
+print("---")
---
+++ test_script_04_stallguard.py
@@ -0,0 +1,134 @@
+from tmc.TMC_2209_StepperDriver import *
+import time
+
+
+print("---")
+print("SCRIPT START")
+print("---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# initiate the TMC_2209 class
+# use your pins for pin_step, pin_dir, pin_en here
+#-----------------------------------------------------------------------
+tmc = TMC_2209(27, 14, 26)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the loglevel of the libary (currently only printed)
+# set whether the movement should be relative or absolute
+# both optional
+#-----------------------------------------------------------------------
+tmc.setLoglevel(Loglevel.info)
+tmc.setMovementAbsRel(MovementAbsRel.absolute)
+
+#     none = 0
+#     error = 10
+#     info = 20
+#     debug = 30
+#     movement = 40
+#     all = 100
+
+
+
+#-----------------------------------------------------------------------
+# these functions change settings in the TMC register
+#-----------------------------------------------------------------------
+tmc.setDirection_reg(False)
+tmc.setVSense(True)
+tmc.setCurrent(600)
+tmc.setIScaleAnalog(True)
+tmc.setInterpolation(True)
+tmc.setSpreadCycle(False)
+tmc.setMicrosteppingResolution(1)
+tmc.setInternalRSense(False)
+
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# these functions read and print the current settings in the TMC register
+#-----------------------------------------------------------------------
+tmc.readIOIN()
+tmc.readCHOPCONF()
+tmc.readDRVSTATUS()
+tmc.readGCONF()
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set the Accerleration and maximal Speed
+#-----------------------------------------------------------------------
+tmc.setAcceleration(2000)
+tmc.setMaxSpeed(500)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# activate the motor current output
+#-----------------------------------------------------------------------
+tmc.setMotorEnabled(True)
+
+
+
+
+
+#-----------------------------------------------------------------------
+# set a callback function for the stallguard interrupt based detection
+# 1. param: pin connected to the tmc DIAG output
+# 2. param: is the threshold StallGuard
+# 3. param: is the callback function (threaded)
+# 4. param (optional): min speed threshold (in steptime measured  in  clock  cycles)
+#-----------------------------------------------------------------------
+def my_callback(channel):  
+    print("StallGuard!")
+    tmc.stop()
+
+tmc.setStallguard_Callback(13, 50, my_callback) # after this function call, StallGuard is active
+
+finishedsuccessfully = tmc.runToPositionSteps(4000, MovementAbsRel.relative)    #move 4000 steps forward
+
+if(finishedsuccessfully == True):
+    print("Movement finished successfully")
+else:
+    print("Movement was not completed")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deactivate the motor current output
+#-----------------------------------------------------------------------
+tmc.setMotorEnabled(False)
+
+print("---\n---")
+
+
+
+
+
+#-----------------------------------------------------------------------
+# deinitiate the TMC_2209 class
+#-----------------------------------------------------------------------
+del tmc
+
+print("---")
+print("SCRIPT FINISHED")
+print("---")
